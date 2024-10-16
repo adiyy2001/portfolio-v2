6a48ca4d8bbd45bbc16c27690bf6d09abac46184
adiy2001 <adiy@onet.pl>
2024-10-16 18:18:57 +0200

    pdf

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Adrian Tubrński',
+    title: 'Adrian Turbński',
     description:
-      'Adrian Tubrński is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Adrian Turbński is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://site.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
   },
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Adrian Tubrński',
-        short_name: 'Adrian Tubrński',
+        name: 'Adrian Turbński',
+        short_name: 'Adrian Turbński',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
